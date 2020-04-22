b11369a907308b7359dcc304c400281780869a58
Roman Grubic <grubic.roman@gmail.com>
2020-04-22 13:07:11 +0000

    Exploring axios package.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -10,9 +10,10 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
+        error: false,
     }
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
@@ -23,23 +24,30 @@ class Blog extends Component {
                 })
                 this.setState({ posts: updatedPosts });
                 // console.log(response)
+            })
+            .catch(error => {
+                this.setState({ error: true });
             });
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.setState({ selectedPostId: id });
     }
 
     render() {
-        const posts = this.state.posts.map(
-            post => {
-                return <Post
-                    key={ post.id }
-                    title={ post.title }
-                    author={ post.author }
-                    clicked={ () => this.postSelectedHandler(post.id) } />
-            }
-        );
+        let posts = <p style={ { textAlign: 'center' } }>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(
+                post => {
+                    return <Post
+                        key={ post.id }
+                        title={ post.title }
+                        author={ post.author }
+                        clicked={ () => this.postSelectedHandler(post.id) } />
+                }
+            );
+        }
+
         return (
             <div>
                 <section className="Posts">

e2f1dd86c7b61ef8f67de8448b96d86850670d60
Roman Grubic <grubic.roman@gmail.com>
2020-04-24 10:10:25 +0000

    Placing routing in place and styling it.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
@@ -13,4 +14,4 @@ const post = (props) => (
     </article>
 );
 
-export default post;
+export default withRouter(post);
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 // import axios from '../../axios';
-import { Route} from 'react-router-dom'
+import { Route, NavLink} from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {    
-
-    
-
-    render() {
-        
-
+    render() {       
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/newPost">New post</a></li>
+                            <li><NavLink 
+                            to="/" 
+                            exact
+                            activeClassName='my-active'
+                            activeStyle={{
+                                color: '#fa923f',
+                                textDecoration: 'underline'
+                            }}>Home</NavLink></li>
+                            <li><NavLink to={{ 
+                                pathname:"/new-post",                                
+                        }}>New post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <Posts />}/>
-                <Route path="/new-post" exact render={() => <Posts />}/>
-
+                {/* <Route path="/" exact render={() => <Posts />}/>
+                <Route path="/new-post" exact render={() => <Posts />}/> */}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" exact component={NewPost}/>
             </div>
         );
     }

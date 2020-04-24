b7f67ea3ad307ea259ff3162abc564882abd6c7a
Roman Grubic <grubic.roman@gmail.com>
2020-04-24 11:26:56 +0000

    Routing and nested routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
 // import axios from '../../axios';
-import { Route, NavLink} from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
-class Blog extends Component {    
-    render() {       
+class Blog extends Component {
+    render() {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                            to="/" 
-                            exact
-                            activeClassName='my-active'
-                            activeStyle={{
-                                color: '#fa923f',
-                                textDecoration: 'underline'
-                            }}>Home</NavLink></li>
-                            <li><NavLink to={{ 
-                                pathname:"/new-post",                                
-                        }}>New post</NavLink></li>
+                            <li><NavLink
+                                to="/posts"
+                                exact
+                                activeClassName='my-active'
+                                activeStyle={ {
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                } }>Posts</NavLink></li>
+                            <li><NavLink to={ {
+                                pathname: "/new-post",
+                            } }>New post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <Posts />}/>
                 <Route path="/new-post" exact render={() => <Posts />}/> */}
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" exact component={NewPost}/>
+                <Switch>
+                    <Route path="/new-post" exact component={ NewPost } />
+                    <Route path="/posts" component={ Posts } />                    
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
+import { Route } from 'react-router-dom';
 
 
 class Posts extends Component {
@@ -28,25 +30,32 @@ class Posts extends Component {
             });
     }
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        this.props.history.push({ pathname: '/posts/' + id });
     }
-    render (){
+    render() {
         let posts = <p style={ { textAlign: 'center' } }>Something went wrong!</p>
         if (!this.state.error) {
             posts = this.state.posts.map(
                 post => {
-                    return <Post
-                        key={ post.id }
-                        title={ post.title }
-                        author={ post.author }
-                        clicked={ () => this.postSelectedHandler(post.id) } />
+                    return (
+                        // <Link to={ '/' + post.id} >
+                        <Post
+                            key={ post.id }
+                            title={ post.title }
+                            author={ post.author }
+                            clicked={ () => this.postSelectedHandler(post.id) } />
+                        // </Link>);
+                    )
                 }
             );
         }
         return (
-            <section className="Posts">
+            <div>
+                <section className="Posts">
                     { posts }
                 </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
         );
     }
 }
